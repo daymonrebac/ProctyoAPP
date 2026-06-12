796a0a00e48ff4e0d89a0ea7cc6400a012604a72
Daymon Rebac <dayrebac@gmail.com>
2026-06-11 21:37:52 -0300

    Improve backup import handling

--- ProctyoAPP/service-worker.js
+++ ProctyoAPP/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "protocolos-v14";
+const CACHE_NAME = "protocolos-v15";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
